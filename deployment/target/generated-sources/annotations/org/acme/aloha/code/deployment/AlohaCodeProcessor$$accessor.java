f39cee82634233e866515060bb795277cd75822e
Andy Damevin <ia3andy@gmail.com>
2021-04-27 11:30:41 +0200

    Add QuarkusCodestartTest (not working)

--- deployment/target/generated-sources/annotations/org/acme/aloha/code/deployment/AlohaCodeProcessor$$accessor.java
+++
@@ -1,7 +0,0 @@
-package org.acme.aloha.code.deployment;
-public final class AlohaCodeProcessor$$accessor {
-    private AlohaCodeProcessor$$accessor() {}
-    public static Object construct() {
-        return new AlohaCodeProcessor();
-    }
-}
--- integration-tests/src/test/java/org/acme/aloha/code/it/AlohaCodeCodestartIT.java
+++ integration-tests/src/test/java/org/acme/aloha/code/it/AlohaCodeCodestartIT.java
@@ -1,20 +1,34 @@
 package org.acme.aloha.code.it;
 
+import static io.quarkus.devtools.codestarts.quarkus.QuarkusCodestartCatalog.Language.JAVA;
+import static io.quarkus.devtools.codestarts.quarkus.QuarkusCodestartCatalog.Language.KOTLIN;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.is;
 
+import io.quarkus.devtools.testing.codestarts.QuarkusCodestartTest;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
 public class AlohaCodeCodestartIT {
 
+    @RegisterExtension
+    public static QuarkusCodestartTest codestartTest = QuarkusCodestartTest.builder()
+            .codestarts("aloha-code")
+            .languages(JAVA, KOTLIN)
+            .build();
+
+
     @Test
-    public void testHelloEndpoint() {
-        given()
-                .when().get("/aloha-code")
-                .then()
-                .statusCode(200)
-                .body(is("Hello aloha-code"));
+    void testContent() throws Throwable {
+        codestartTest.checkGeneratedSource("org.acme.MyHelloExample");
+    }
+
+    @Test
+    @EnabledIfSystemProperty(named = "build-projects", matches = "true")
+    void buildAllProjectsForLocalUse() throws Throwable {
+        codestartTest.buildAllProjects();
     }
 }
